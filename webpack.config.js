629a8c143418531b7e2f59d0128e28b1a9b167d1
Exquisite7 <razvanmanea07@gmail.com>
2024-07-24 15:45:57 +0300

    eeee

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,15 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash:8].[ext]',
+              outputPath: 'images/',
+            },
+          },
+        ],
       },
     ],
   },
